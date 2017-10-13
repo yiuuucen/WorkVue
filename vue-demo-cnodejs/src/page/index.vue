c81ef8ec874d6b7a4495430c6497c520a98809f5
yiuuucen <1784373887@qq.com>
2017-10-13 17:03:34 +0800

    work

---
+++ vue-demo-cnodejs/src/page/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>index page</div>
+</template>
+<script>
+  export default {
+    created () {
+      this.$api.get('topics', null, r => {
+              console.log(r)
+           }
+       )
+    }
+  }
+</script>
